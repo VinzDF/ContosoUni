69e8ec491e0a5a00222715adea15ae28b714e96a
VinzDF <de.falco.vinz@gmail.com>
2024-03-21 17:02:55 +0100

    Contoso University

---
+++ ContosoUniversity/Migrations/20240314122412_InitCreate.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class InitCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Department_DepartmentID",
+                table: "Course");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Instructor_InstructorID",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignment_Instructor_InstructorID",
+                table: "OfficeAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OfficeAssignment",
+                table: "OfficeAssignment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Department",
+                table: "Department");
+
+            migrationBuilder.RenameTable(
+                name: "OfficeAssignment",
+                newName: "OfficeAssignments");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollment",
+                newName: "Enrollments");
+
+            migrationBuilder.RenameTable(
+                name: "Department",
+                newName: "Departments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollment_StudentID",
+                table: "Enrollments",
+                newName: "IX_Enrollments_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollment_CourseID",
+                table: "Enrollments",
+                newName: "IX_Enrollments_CourseID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Department_InstructorID",
+                table: "Departments",
+                newName: "IX_Departments_InstructorID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "Enrollments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OfficeAssignments",
+                table: "OfficeAssignments",
+                column: "InstructorID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments",
+                column: "EnrollmentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments",
+                column: "DepartmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Departments_DepartmentID",
+                table: "Course",
+                column: "DepartmentID",
+                principalTable: "Departments",
+                principalColumn: "DepartmentID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Instructor_InstructorID",
+                table: "Departments",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Course_CourseID",
+                table: "Enrollments",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Student_StudentID",
+                table: "Enrollments",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignments_Instructor_InstructorID",
+                table: "OfficeAssignments",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Departments_DepartmentID",
+                table: "Course");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Instructor_InstructorID",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Course_CourseID",
+                table: "Enrollments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Student_StudentID",
+                table: "Enrollments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OfficeAssignments_Instructor_InstructorID",
+                table: "OfficeAssignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OfficeAssignments",
+                table: "OfficeAssignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "OfficeAssignments",
+                newName: "OfficeAssignment");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollments",
+                newName: "Enrollment");
+
+            migrationBuilder.RenameTable(
+                name: "Departments",
+                newName: "Department");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollments_StudentID",
+                table: "Enrollment",
+                newName: "IX_Enrollment_StudentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollments_CourseID",
+                table: "Enrollment",
+                newName: "IX_Enrollment_CourseID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Departments_InstructorID",
+                table: "Department",
+                newName: "IX_Department_InstructorID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Grade",
+                table: "Enrollment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OfficeAssignment",
+                table: "OfficeAssignment",
+                column: "InstructorID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment",
+                column: "EnrollmentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Department",
+                table: "Department",
+                column: "DepartmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Department_DepartmentID",
+                table: "Course",
+                column: "DepartmentID",
+                principalTable: "Department",
+                principalColumn: "DepartmentID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Instructor_InstructorID",
+                table: "Department",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Course_CourseID",
+                table: "Enrollment",
+                column: "CourseID",
+                principalTable: "Course",
+                principalColumn: "CourseID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollment_Student_StudentID",
+                table: "Enrollment",
+                column: "StudentID",
+                principalTable: "Student",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OfficeAssignment_Instructor_InstructorID",
+                table: "OfficeAssignment",
+                column: "InstructorID",
+                principalTable: "Instructor",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
